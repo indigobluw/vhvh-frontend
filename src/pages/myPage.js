6b0c96b0627952a087db245a5f14e97e91006fcc
Sofia <sofia.montgomery@gmail.com>
2023-05-11 23:07:10 +0200

    add a logout button where token is removed and header is set to null

--- src/pages/myPage.js
+++ src/pages/myPage.js
@@ -1,6 +1,6 @@
 import styles from "src/styles/MyPage.module.scss";
 import Navbar from "@/components/Navbar/Navbar";
-
+import LogoutButton from "@/components/Logout/LogoutButton";
 
 export default function CreateAccount() {
   return (
@@ -8,6 +8,7 @@ export default function CreateAccount() {
       <Navbar />
       <div>
         <h2>Välkommen!</h2>
+        <LogoutButton />
       </div>
     </div>
   );
