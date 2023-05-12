21fb0f0c2d6acf875135c678aebe5b3e43e87e17
Therese Lindquist <91468456+indigobluw@users.noreply.github.com>
2023-05-12 18:42:30 +0200

    Switch navbar to mui Appbar, hamburger menu works

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,34 @@
 import styles from "src/components/Navbar/Navbar.module.scss";
 import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState, useRef, useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
 
 export default function Navbar() {
+  const [phoneMenuOpen, setPhoneMenuOpen] = useState(false);
+
+  const openMenu = () => {
+    setPhoneMenuOpen(!phoneMenuOpen);
+  };
+
+  const onblur = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutsideMenu(event) {
+      if (onblur.current && !onblur.current.contains(event.target)) {
+        setPhoneMenuOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutsideMenu);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideMenu);
+    };
+  }, [onblur]);
+
   return (
-    <div className={styles.navbar}>
-      <navbar>
+    <div>
+      <AppBar className={styles.navbar}>
         <ul className={styles.ul}>
           <Link href="/">
             <li className={styles.li}>Startsida</li>
@@ -17,10 +40,33 @@ export default function Navbar() {
             <li className={styles.li}>Om oss</li>
           </Link>
           <li className={styles.burger}>
-            <MenuIcon fontSize="large" />
+            {phoneMenuOpen ? (
+              <CloseIcon fontSize="large" onClick={openMenu} />
+            ) : (
+              <MenuIcon fontSize="large" onClick={openMenu} />
+            )}
           </li>
         </ul>
-      </navbar>
+        <div
+          ref={onblur}
+          className={`${styles.hamburgermenu} ${
+            phoneMenuOpen ? styles.active : ""
+          }`}
+          onClick={openMenu}
+        >
+          <ul>
+            <Link href="/">
+              <li className={styles.menuItem}>Startsida</li>
+            </Link>
+            <Link href="/comingSoon">
+              <li className={styles.menuItem}>Kontakt</li>
+            </Link>
+            <Link href="/comingSoon">
+              <li className={styles.menuItem}>Om oss</li>
+            </Link>
+          </ul>
+        </div>
+      </AppBar>
     </div>
   );
 }
