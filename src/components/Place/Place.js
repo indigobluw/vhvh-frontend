aec0f3fc66c70480f64de01a0b44e4f0e96904f2
Therese Lindquist <91468456+indigobluw@users.noreply.github.com>
2023-05-12 10:40:34 +0200

    PAIR PROGRAMMING, added Place component

---
+++ src/components/Place/Place.js
@@ -0,0 +1,13 @@
+import styles from "src/components/Place/Place.module.scss";
+import AddIcon from "@mui/icons-material/Add";
+import IconButton from "@mui/material/IconButton";
+
+export default function Place() {
+  return (
+    <div>
+      <div className={styles.container}>
+        <h3>Min plats</h3>
+      </div>
+    </div>
+  );
+}
