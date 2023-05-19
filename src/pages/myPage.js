6f85120b3327b5e6a322071e2af81c2ed86245a5
Sofia <sofia.montgomery@gmail.com>
2023-05-19 13:44:36 +0200

    added userinformation and change some user information function

--- src/pages/myPage.js
+++ src/pages/myPage.js
@@ -2,12 +2,11 @@ import styles from "src/styles/MyPage.module.scss";
 import Navbar from "@/components/Navbar/Navbar";
 import LogoutButton from "@/components/Logout/LogoutButton";
 import Place from "@/components/Place/Place";
+import MyData from "@/components/MyData/MyData";
+import Welcome from "@/components/Welcome/Welcome";
 import jwt from "jsonwebtoken";
 
 export default function MyPage() {
-
-  
-
   return (
     <div>
       <title> Min sida | VHVH </title>
@@ -20,10 +19,15 @@ export default function MyPage() {
           <h2>Min sida</h2>
         </div>
         <div className={styles.resten}>
+          <div className={styles.welcome}>
+            <Welcome />
+          </div>
           <div className={styles.places}>
             <Place />
           </div>
-          <div className={styles.myData}>Mina användaruppgifter</div>
+          <div className={styles.myData}>
+            <MyData />
+          </div>
         </div>
       </div>
     </div>
